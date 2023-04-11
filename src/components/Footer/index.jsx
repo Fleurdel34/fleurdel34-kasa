16c7956f210bccd10bb44ba72262a1014ea151a5
Fleurdel34 <betdevelweb0953@gmail.com>
2023-04-11 15:16:07 +0200

    Mise en place du header de la banner et du footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,35 @@
-/*import styled from "styled-components"*/
+import styled from "styled-components"
 import LOGO from '../images/LOGO.png';
+
+const FooterDiv = styled.div`
+    background:#000000;
+    display:column;
+    justify-content:space-around;
+    height:209px;
+    text-align:center;
+
+`
+const FooterImg = styled.img`
+        margin-top:4rem;
+
+`
+
+const FooterParagraph = styled.p`
+    color:#fff;
+    font-family: 'Montserrat';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+
+`
 
 
 function Footer(){
     return(
-        <div className='footer'>
-                <img src= {LOGO} alt="Logo"/>
-                <p>© 2020 Kasa. All rights reserved</p> 
-        </div>
+        <FooterDiv>
+            <FooterImg src= {LOGO} alt="Logo"/>
+            <FooterParagraph>© 2020 Kasa. All rights reserved</FooterParagraph> 
+        </FooterDiv>
         
     )
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,45 @@
 import { Link } from "react-router-dom";
 import LOGO1 from '../images/LOGO1.png';
-/*import styled from "styled-components"*/
+import styled from "styled-components"
+
+const HeaderDiv = styled.div`
+    display:flex;
+    margin-top:2rem;
+    width:100%;
+    padding-left:7rem;
+    
+`
+const LinkDiv = styled.div`
+    width:50%;
+    display:flex;
+    justify-content:flex-end;
+    align-self:center;
+    text-align: right;
+    padding-left:23rem;
+`
+
+const LinkStyle = styled(Link)`
+    display:flex;
+    justify-content:flex-end;
+    color:#ff6060;
+    width:25%;
+    font-size:24px;
+    font-family:Montserrat;
+    font-style: normal;
+    font-weight: 500;
+    
+`
+
 
 function Header(){
     return(
-        <div className="header">
-            <div className="image_logo">
-             <img src= {LOGO1} alt="Logo"/>
-            </div>
-            <div className="menu_nav">
-                <nav>
-                    <Link to="/">Accueil</Link>
-                    <Link to="/apropos">A propos</Link>
-                </nav>
-            </div>
-        </div>
+        <HeaderDiv>
+                <img src= {LOGO1} alt="Logo"/>
+            <LinkDiv>
+                <LinkStyle to="/">Accueil</LinkStyle>
+                <LinkStyle to="/apropos">A propos</LinkStyle>
+            </LinkDiv>
+        </HeaderDiv>
     )
 }
 export default Header
